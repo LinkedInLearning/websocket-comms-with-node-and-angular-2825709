e0c73307cfac3810fcc513c813e35c1b587fc28d
RoxKilly <4ibi6jrdj3ynzgxy@protonmail.com>
2022-01-01 17:57:43 -0500

    [play] webapp: Add mock chat messages

--- packages/webapp/src/app/app.component.ts
+++ packages/webapp/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User, ServerChatMessage } from '@websocket/types';
 
 @Component({
   selector: 'websocket-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webapp';
+  user: User;
+  messages: ServerChatMessage[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.user = { name: 'John', id: 1}
+    this.messages = [
+      {
+        event: 'chat',
+        payload: {
+          author: {
+            name: 'Jane',
+            id: 2
+          },
+          contents: 'Hi I am Jane'
+        }
+      },
+      {
+        event: 'chat',
+        payload: {
+          author: {
+            name: 'Jane',
+            id: 2
+          },
+          contents: 'Anybody here?'
+        }
+      },
+      {
+        event: 'chat',
+        payload: {
+          author: {...this.user},
+          contents: 'Yes I am here!'
+        }
+      },
+    ]
+  }
 }
--- packages/webapp/src/app/app.module.ts
+++ packages/webapp/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
