f100ee0e4b403a9ef442858810b8183881231f9c
RoxKilly <4ibi6jrdj3ynzgxy@protonmail.com>
2022-01-06 16:05:45 -0500

    Mock data; Add CSS grid

--- packages/webapp/src/app/app.component.ts
+++ packages/webapp/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatRelayMessage, User } from '@websocket/types';
 
 @Component({
   selector: 'websocket-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'I am Angular';
+
+  messages: ChatRelayMessage[] = []
+  currentUser: User
+
+  ngOnInit() {
+    this.messages = [
+      { event: 'chatRelay', author: { name: 'Jane', id: 1 }, contents: 'Hi this is Jane' },
+      { event: 'chatRelay', author: { name: 'Henry', id: 2 }, contents: 'Hello I am Henry' }
+    ]
+
+    this.currentUser = { name: 'Xavier', id: 3 }
+  }
 }
