e1b19f578f672d9af87a6901f47e184d8a9b3201
RoxKilly <4ibi6jrdj3ynzgxy@protonmail.com>
2022-01-05 23:25:27 -0500

    First message exchange

--- packages/server/src/app/ws-handler.ts
+++ packages/server/src/app/ws-handler.ts
@@ -1,4 +1,4 @@
-import { ServerOptions, WebSocket, WebSocketServer } from 'ws'
+import { RawData, ServerOptions, WebSocket, WebSocketServer } from 'ws'
 import { IncomingMessage } from 'http';
 
 export class WsHandler {
@@ -13,5 +13,20 @@ export class WsHandler {
 
   onSocketConnected(socket: WebSocket, request: IncomingMessage) {
     console.log(`New websocket connection!`)
+
+    socket.on('message', (data) => this.onSocketMessage(socket, data))
+    socket.on('close', ((code, reason) => this.onSocketClosed(code, reason)))
+  }
+
+  onSocketMessage(socket: WebSocket, data: RawData) {
+    const payload = JSON.parse(`${data}`)
+    console.log(`Received: `, payload)
+
+    const reply = JSON.stringify({ reply: 'Message received' })
+    socket.send(reply)
+  }
+
+  onSocketClosed(code: number, reason: Buffer) {
+    console.log(`Client has disconnected; code=${code}, reason=${reason}`)
   }
 }
