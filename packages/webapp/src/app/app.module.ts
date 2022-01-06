72d0312511b4c50660fdc3e9fe9bc10012bce8ba
RoxKilly <4ibi6jrdj3ynzgxy@protonmail.com>
2022-01-06 16:53:48 -0500

    Chat Card and Input

--- packages/webapp/src/app/app.module.ts
+++ packages/webapp/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
